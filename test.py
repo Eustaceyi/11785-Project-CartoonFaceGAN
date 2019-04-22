d673a5ddf6fe67bd413ac586707d861878bc0058
yuyangwang95@gmail.com <yuyangwang95@gmail.com>
2019-04-22 15:52:51 -0400

    add cycle gan

--- test.py
+++ test.py
@@ -11,21 +11,27 @@ test_in2 = np.random.randn(2, 3, 128, 128)
 test_in2 = torch.Tensor(test_in2)
 print(test_in1.shape)
 
-model = ResnetGenerator(in_channels=3, out_channels=3, n_filters=4, n_blocks=1, use_dropout=False)
+G = ResnetGenerator(in_channels=3, out_channels=3, n_filters=4, n_blocks=1, use_dropout=False)
+F = ResnetGenerator(in_channels=3, out_channels=3, n_filters=4, n_blocks=1, use_dropout=False)
 
-test_out1 = model(test_in1)
-test_out2 = model(test_in2)
+test_out1 = G(test_in1)
+test_out2 = F(test_out1)
 
 gan_criterion = GANLoss()
 cycle_criterion = CycleLoss()
+identity_criterion = IdentityLoss()
 
-optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=5e-5)
+optimizer = optim.Adam(G.parameters(), lr=1e-3, weight_decay=5e-5)
+gan_loss = gan_criterion(test_out1, is_real=False)
+cycle_loss = cycle_criterion(test_out1, test_out2)
+identity_loss = identity_criterion(test_out1, test_in1)
 # loss1 = gan_criterion(test_out1, True)
 # loss1.backward()
 # optimizer.step()
 
-loss2 = cycle_criterion(test_out1, test_out2)
-loss2.backward()
+loss = gan_loss + cycle_loss + identity_loss
+loss.backward()
 optimizer.step()
 
+print(loss.item())
 print(test_out1.shape)
