43b8cdf2618e24f06124b5fdb0792cccc3cfd469
yuyangwang95@gmail.com <yuyangwang95@gmail.com>
2019-04-25 21:55:44 -0400

    update loss

--- model/loss.py
+++ model/loss.py
@@ -1,26 +1,49 @@
 import numpy as np
 import torch
-
 import torch.nn as nn
 
 
 class GANLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, target_real_label=1.0, target_fake_label=0.0):
         super(GANLoss, self).__init__()
-        self.loss = nn.BCEWithLogitsLoss()
-        self.real_label = torch.tensor(1.0)
-        self.fake_label = torch.tensor(0.0)
+        self.register_buffer('real_label', torch.tensor(target_real_label))
+        self.register_buffer('fake_label', torch.tensor(target_fake_label))
+        self.gan_mode = gan_mode
+        self.loss = nn.MSELoss()
 
-    def __get_target_tensor(self, predictions, is_real):
-        target_tensor = self.real_label if is_real else self.fake_label
-        return target_tensor.expand_as(predictions).cuda()
+    def get_target_tensor(self, prediction, target_is_real):
+        if target_is_real:
+            target_tensor = self.real_label
+        else:
+            target_tensor = self.fake_label
+        return target_tensor.expand_as(prediction)
+
+    def __call__(self, prediction, target_is_real):
+        return self.forward(prediction, target_is_real)
     
-    def __call__(self, predictions, is_real):
-        return self.forward(predictions, is_real)
+    def forward(self, prediction, target_is_real):
+        target_tensor = self.get_target_tensor(prediction, target_is_real)
+        loss = self.loss(prediction, target_tensor)
+        return loss
 
-    def forward(self, predictions, is_real):
-        target_tensor = self.__get_target_tensor(predictions, is_real)
-        return self.loss(predictions, target_tensor)
+
+# class GANLoss(nn.Module):
+#     def __init__(self):
+#         super(GANLoss, self).__init__()
+#         self.loss = nn.BCEWithLogitsLoss()
+#         self.real_label = torch.tensor(1.0)
+#         self.fake_label = torch.tensor(0.0)
+
+#     def __get_target_tensor(self, predictions, is_real):
+#         target_tensor = self.real_label if is_real else self.fake_label
+#         return target_tensor.expand_as(predictions).cuda()
+    
+#     def __call__(self, predictions, is_real):
+#         return self.forward(predictions, is_real)
+
+#     def forward(self, predictions, is_real):
+#         target_tensor = self.__get_target_tensor(predictions, is_real)
+#         return self.loss(predictions, target_tensor)
 
 
 class CycleLoss(nn.L1Loss):
