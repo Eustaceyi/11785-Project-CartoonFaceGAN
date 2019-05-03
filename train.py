8d58e74b81b8d2aa196e29c775687aa967a10918
Jundong Yi <eustaceyi@gmail.com>
2019-05-02 20:19:45 -0400

    Update train.py

--- train.py
+++ train.py
@@ -4,29 +4,7 @@ from util.visualizer import Visualizer
 from util.utils import save_img
 from util.dataloader import *
 from CycleGAN import *
-
-
-def tensor2im(input_image, imtype=np.uint8):
-    """"Converts a Tensor array into a numpy image array.
-
-    Parameters:
-        input_image (tensor) --  the input image tensor array
-        imtype (type)        --  the desired type of the converted numpy array
-    """
-    if not isinstance(input_image, np.ndarray):
-        if isinstance(input_image, torch.Tensor):  # get the data from a variable
-            image_tensor = input_image.data
-        else:
-            return input_image
-        image_numpy = image_tensor[0].cpu().float().numpy()  # convert it into a numpy array
-        if image_numpy.shape[0] == 1:  # grayscale to RGB
-            image_numpy = np.tile(image_numpy, (3, 1, 1))
-        image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0  # post-processing: tranpose and scaling
-    else:  # if it is a numpy array, do nothing
-        image_numpy = input_image
-    image_numpy.astype(imtype)
-    return image_numpy.transpose([2,0,1])
-
+from util.utils import tensor2im
 
 if __name__ == '__main__':   # get training options
     dataset = loader_main(opt)        # create a dataset given opt.dataset_mode and other options
